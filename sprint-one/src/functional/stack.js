c4fea92940b7088d7ebc39ebe76e5b1150b70873
Wyatt Fry <wyattfry@gmail.com>
2018-01-17 11:16:59 -0800

    passes first 3 tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,6 +6,7 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[Object.keys(storage).length] = value;
   };
 
   someInstance.pop = function() {
